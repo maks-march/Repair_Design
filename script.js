df59e4d7d82075b59dd53eba11cc4765656a67c3
maks-march <98648053+maks-march@users.noreply.github.com>
2024-06-06 23:40:53 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,27 +4,12 @@ const city = document.getElementById("city");
 const time = document.getElementById("time");
 const size = document.getElementById("size");
 const cost = document.getElementById("cost");
-const path = "img/";
 let current_type = 0;
-let current_img = 0;
 var cities = ["Rostov-on-Don<br>LCD admiral", "Sochi", "Rostov-on-Don<br>LCD patriotic"];
 var sizes = ["81 M2", "150 M2", "180 M2"];
 var times = ["3.5 months", "5 months", "10 months"];
 var costs = ["Upon request", "10000$", "25288$"];
-var types = ["don_admiral", "sochi", "don_patriotic"];
-var img = ["1.jpg", "2.jpg", "3.jpg"];
-for (let i = 0; i < type_btns.length; i++) {
-    type_btns[i].addEventListener("click", () => {
-        type_btns[current_type].classList.remove("type_active");
-        type_btns[i].classList.add("type_active");
-        current_type = i;
-        image.src = path+types[current_type]+img[current_img];
-        city.innerHTML = cities[current_type];
-        size.innerHTML = sizes[current_type];
-        time.innerHTML = times[current_type];
-        cost.innerHTML = costs[current_type];
-    });
-}
+var paths = ["img/don_admiral.jpg", "img/sochi.jpg", "img/don_patriotic.jpg"];
 
 const left_btn = document.getElementById("left");
 const right_btn = document.getElementById("right");
@@ -36,19 +21,32 @@ const unactive_dot = "img/unactive_dot.svg";
 var dots = [dot1, dot2, dot3];
 
 right_btn.addEventListener("click", () => {
-    if (current_img != 2) {
-        dots[current_img].src = unactive_dot;
-        current_img++;
-        dots[current_img].src = active_dot;
-        image.src = path+types[current_type]+img[current_img];
+    if (current_type != 2) {
+        dots[current_type].src = unactive_dot;
+        type_btns[current_type].classList.remove("type_active");
+        current_type++;
+        type_btns[current_type].classList.add("type_active");
+        dots[current_type].src = active_dot;
+        image.src = paths[current_type];
+        city.innerHTML = cities[current_type];
+        size.innerHTML = sizes[current_type];
+        time.innerHTML = times[current_type];
+        cost.innerHTML = costs[current_type];
     }
+    
 });
 
 left_btn.addEventListener("click", () => {
-    if (current_img != 0) {
-        dots[current_img].src = unactive_dot;
-        current_img--;
-        dots[current_img].src = active_dot;
-        image.src = path+types[current_type]+img[current_img];
+    if (current_type != 0) {
+        dots[current_type].src = unactive_dot;
+        type_btns[current_type].classList.remove("type_active");
+        current_type--;
+        type_btns[current_type].classList.add("type_active");
+        dots[current_type].src = active_dot;
+        image.src = paths[current_type];
+        city.innerHTML = cities[current_type];
+        size.innerHTML = sizes[current_type];
+        time.innerHTML = times[current_type];
+        cost.innerHTML = costs[current_type];
     }
 });
